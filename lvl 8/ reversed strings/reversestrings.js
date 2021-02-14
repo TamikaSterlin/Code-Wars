52c03409f0de7bd4d8a9a2b229f861e9f2a7b81c
TamikaSterlin <77872996+TamikaSterlin@users.noreply.github.com>
2021-02-13 19:21:22 -0500

    Update reversestrings.js

--- lvl 8/ reversed strings/reversestrings.js
+++ lvl 8/ reversed strings/reversestrings.js
@@ -1,3 +1,10 @@
+
+//Reversed Strings - 
+//Complete the solution so that it reverses the string passed into it.
+
+
+
+
 function solution(str) {
   let newString = ''
   for (let i = str.length - 1; i >= 0; i--) {
