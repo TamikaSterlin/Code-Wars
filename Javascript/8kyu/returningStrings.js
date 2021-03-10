425ce0280a05d9e7f5a503a8acae340192146d73
tamika sterlin <sterlintamika@gmail.com>
2021-03-09 20:59:58 -0500

    codewars 3/9

---
+++ Javascript/8kyu/returningStrings.js
@@ -0,0 +1,14 @@
+/*Make a function
+that will return a greeting statement that uses an input; your
+program should return, "Hello, <name> how are you doing today?".*/
+
+
+
+
+function greet(name){
+  let greeting = 'Hello, ' + name  + " how are you doing today?"
+  return greeting
+
+}
+
+greet('tamika')
