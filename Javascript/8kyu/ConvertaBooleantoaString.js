0ff603b4af6728cf7a936c458b4b47d4a10b6123
tamika sterlin <sterlintamika@gmail.com>
2021-03-10 21:54:18 -0500

    codewar for 3/10

---
+++ Javascript/8kyu/ConvertaBooleantoaString.js
@@ -0,0 +1,11 @@
+//Implement a function which convert the given boolean value into its string representation.
+
+
+
+function booleanToString(b){
+return b.toString()
+}
+
+//or
+
+const booleanToString = b => b.toString()
