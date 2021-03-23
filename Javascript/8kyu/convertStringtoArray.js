2727258b0a4a13cd7bbb84cad79c275f2c060c6d
tamika sterlin <sterlintamika@gmail.com>
2021-03-22 20:59:46 -0400

    codewar 3/22

---
+++ Javascript/8kyu/convertStringtoArray.js
@@ -0,0 +1,8 @@
+//Write a function to split a string and convert it into an array of words. For example:
+
+function stringToArray(string){
+
+	// code code code
+ return string.trim().split(" ")
+
+}
