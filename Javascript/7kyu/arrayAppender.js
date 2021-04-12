b74098ae599f097be08fff2c60247a9bf14c3cbd
tamika sterlin <sterlintamika@gmail.com>
2021-04-12 17:52:57 -0400

    codewar 4/12

---
+++ Javascript/7kyu/arrayAppender.js
@@ -0,0 +1,8 @@
+//Write a function that appends the items from sequence 2 onto sequence 1,
+//returning the newly formed sequence. Your function should also be able to handle nested sequences.
+
+function appendArrays (arr1, arr2) {
+
+    // your solution here...
+  return arr1.concat(arr2)
+}
